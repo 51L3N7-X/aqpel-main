8e812efa452446b80e810d117dbda837b5d75b4b
51l3n7 <silentforthewin@proton.me>
2024-03-28 02:34:30 +0300

    test

--- apps/dashboard/src/app/(auth)/signin/page.tsx
+++ apps/dashboard/src/app/(auth)/signin/page.tsx
@@ -5,7 +5,7 @@ import SignInForm from "@/components/auth/SignInForm";
 
 export default function SingUp() {
   return (
-    <div className="relative h-[100dvh] w-[100vw] ">
+    <div className="relative  h-dvh w-screen ">
       <div className=" absolute size-full bg-[url('/loginBackGround.jpg')] bg-cover bg-center bg-no-repeat" />
       <div className=" absolute z-10 size-full bg-[#00000078]" />
       <LogorsignCard
