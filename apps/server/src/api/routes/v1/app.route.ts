f04cf946ca75417d204a8f33a27f56c23c00edd7
51l3n7 <abdullah22shiban42@gmail.com>
2024-05-15 23:53:05 +0300

    fix(api): delete some unused routes

--- apps/server/src/api/routes/v1/app.route.ts
+++ apps/server/src/api/routes/v1/app.route.ts
@@ -1,13 +1,13 @@
 import express from "express";
 export const router = express.Router();
 
-import { login } from "../../controllers/app/login.controller";
+// import { login } from "../../controllers/app/login.controller";
 import { getOrders } from "../../controllers/app/getOrders.controller";
 
 import { getTables } from "../../controllers/app/getTables.controller";
 import { auth } from "../../middlewares/auth";
 
-router.post("/login", login);
+// router.post("/login", login);
 
 router.use(auth());
 
