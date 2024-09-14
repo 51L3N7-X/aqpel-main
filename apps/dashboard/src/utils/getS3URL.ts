b9bb35a00b166c84cde0d67d8d4cbf21bfef540a
51l3n7 <abdullah22shiban42@gmail.com>
2024-09-14 15:05:57 +0300

    fix(api): add public path

--- apps/dashboard/src/utils/getS3URL.ts
+++ apps/dashboard/src/utils/getS3URL.ts
@@ -2,7 +2,7 @@ import api from "@/lib/api";
 
 export const getS3URL = async (): Promise<string> => {
   const requestURL = await api({
-    url: "/getUploadURL",
+    url: "/user/getUploadURL",
     method: "get",
     params: { fileExtension: "png" },
   });
